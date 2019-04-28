6d6a275800001024545d34ed509c263c53ddbe22
Denise Hung <denisehung87@gmail.com>
2019-04-28 13:57:01 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -27,11 +27,6 @@ function ternaryTeenager(age) {
   "You are not a teenager";
 }
 
-function switchAge(age);
-  switch(age) {
-    case >=13 && <=19:
-      console.log("You are a teenager");
-      break;
-    default:
-      console.log("You have an age");
+function switchAge(age){
+
 }
